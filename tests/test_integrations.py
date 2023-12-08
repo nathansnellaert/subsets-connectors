26a02801469775d1272fdcf434d818fe34969afe
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2023-12-08 17:01:01 +0100

    Clean up test file

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1,7 +1,5 @@
-import pytest
 from integrations import defs
 
-print(defs)
 def test_definitions_loaded():
     assert len(defs.get_asset_graph().assets) > 0, "There should be at least one asset defined"
     assert len(defs.get_all_job_defs()) > 0, "There should be at least one job defined"
