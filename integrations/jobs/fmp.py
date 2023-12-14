414c78eae5ae00d86264f307ea8794773173859d
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2023-12-14 14:25:00 +0100

    Set job level resources

--- integrations/jobs/fmp.py
+++ integrations/jobs/fmp.py
@@ -45,10 +45,26 @@ import integrations.assets.fmp.analysts as analysts_assets
 
 fmp_eod_assets = [fmp_assets.fmp_eod_prices, fmp_assets.indices_prices, fmp_assets.commodity_prices, fmp_assets.cryptocurrency_prices, fmp_assets.forex_prices]
 
-fmp_eod_job = define_asset_job(
-    name='fmp_eod_assets',
+daily_partition_job = define_asset_job(
+    name='daily_partition_job',
     selection=fmp_eod_assets,
-    tags={"concurrency_group": "fmp"}
+    tags={
+        "concurrency_group": "fmp",
+        "dagster-k8s/config": {
+            "container_config": {
+                "resources": {
+                    "requests": {
+                        "cpu": "50m",
+                        "memory": "128Mi"
+                    },
+                    "limits": {
+                        "cpu": "50m",
+                        "memory": "128Mi"
+                    }
+                }
+            }
+        }
+    }
 )
 
 fmp_unpartitioned_assets = load_assets_from_modules(
