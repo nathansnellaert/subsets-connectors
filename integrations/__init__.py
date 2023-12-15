fb956b3eec789c8fb7409a8a81709691e5138a74
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2023-12-15 12:51:26 +0100

    Rename fmp eod asset job

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -7,10 +7,10 @@ from integrations.io_managers.gcs_pyarrow_io_manager import GCSPyArrowIOManager
 from integrations.io_managers.local_pandas_io_manager import LocalPandasIOManager
 from integrations.io_managers.local_pyarrow_io_manager import LocalPyArrowIOManager
 from integrations.jobs.fmp import (
-    daily_partition_job as fmp_daily_partition_job,
-    daily_partition_assets as fmp_daily_partition_assets,
-    unpartitioned_assets as fmp_unpartitioned_assets,
-    unpartitioned_job as fmp_unpartitioned_job
+    fmp_eod_assets,
+    fmp_eod_job,
+    fmp_unpartitioned_assets,
+    fmp_unpartitioned_assets_job
 )
 from integrations.jobs.fred import job as fred_job, assets as fred_assets
 from integrations.jobs.regular import job as regular_job, assets as regular_assets
@@ -23,8 +23,8 @@ pandas_io_manager = LocalPandasIOManager() if ENV == 'test' else GCSPandasIOMana
 pyarrow_io_manager = LocalPyArrowIOManager() if ENV == 'test' else GCSPyArrowIOManager(os.environ['GCS_BUCKET'])
 
 defs = Definitions(
-    assets=fmp_daily_partition_assets + fmp_unpartitioned_assets + fmp_unpartitioned_assets + fred_assets + regular_assets + wikipedia_assets + internal_assets,
-    jobs=[fmp_daily_partition_job, fmp_unpartitioned_job, fmp_unpartitioned_job, fred_job, regular_job, wikipedia_job, internal_job],
+    assets=fmp_eod_assets + fmp_unpartitioned_assets + fred_assets + regular_assets + wikipedia_assets + internal_assets,
+    jobs=[fmp_eod_job, fmp_unpartitioned_assets_job, fred_job, regular_job, wikipedia_job, internal_job],
     resources={
         "io_manager": pandas_io_manager,
         "vanilla_parquet_io_manager": pyarrow_io_manager 
