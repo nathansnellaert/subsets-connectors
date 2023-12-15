053de3c919c14153ae508e03d301b6997f464f70
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2023-12-15 12:51:14 +0100

    Start eod prices datasets at 2014

--- integrations/assets/fmp/end_of_day_prices.py
+++ integrations/assets/fmp/end_of_day_prices.py
@@ -20,40 +20,40 @@ def make_request(path) -> pd.DataFrame:
     df = pd.read_csv(url)
     return df
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def fmp_eod_prices(context) -> pd.DataFrame:
     path = f'batch-request-end-of-day-prices?date={context.partition_key}&apikey=' + os.environ['FMP_API_KEY']
     return make_request(path)
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def cryptocurrency_prices(fmp_crypto_symbols: pd.DataFrame, fmp_eod_prices: pd.DataFrame) -> pd.DataFrame:
     crypto_symbols = fmp_crypto_symbols['symbol'].unique()
     crypto_prices = fmp_eod_prices[fmp_eod_prices['symbol'].isin(crypto_symbols)]
     return crypto_prices
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def commodity_prices(fmp_commodity_symbols: pd.DataFrame, fmp_eod_prices: pd.DataFrame) -> pd.DataFrame:
     commodity_symbols = fmp_commodity_symbols['symbol'].unique()
     commodity_prices = fmp_eod_prices[fmp_eod_prices['symbol'].isin(commodity_symbols)]
     return commodity_prices
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def forex_prices(fmp_forex_symbols: pd.DataFrame, fmp_eod_prices: pd.DataFrame) -> pd.DataFrame:
     forex_symbols = fmp_forex_symbols['symbol'].unique()
     forex_prices = fmp_eod_prices[fmp_eod_prices['symbol'].isin(forex_symbols)]
     return forex_prices
 
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def indices_prices(fmp_indices_symbols: list, fmp_eod_prices: pd.DataFrame) -> pd.DataFrame:
     indices_symbols = [symbol for symbol, name in fmp_indices_symbols]
     indices_prices = fmp_eod_prices[fmp_eod_prices['symbol'].isin(indices_symbols)]
     return indices_prices
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2013-01-01"))
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2014-01-01"))
 def etf_prices(fmp_etf_symbols: list, fmp_eod_prices: pd.DataFrame) -> pd.DataFrame:
     etf_symbols = [symbol for symbol, name in fmp_etf_symbols]
     etf_prices = fmp_eod_prices[fmp_eod_prices['symbol'].isin(etf_symbols)]
