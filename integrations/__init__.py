fb5b8ad696d679b9a05310737d7c255c2d73d74b
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2023-12-19 19:16:56 +0100

    Create separate job for unctad assets

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -16,15 +16,35 @@ from integrations.jobs.fred import job as fred_job, assets as fred_assets
 from integrations.jobs.regular import job as regular_job, assets as regular_assets
 from integrations.jobs.wikipedia import job as wikipedia_job, assets as wikipedia_assets
 from integrations.jobs.internal import job as internal_job, assets as internal_assets
+from integrations.jobs.unctad import job as unctad_job, assets as unctad_assets
 
 ENV = os.environ.get("ENV", "dev")
 
 pandas_io_manager = LocalPandasIOManager() if ENV == 'test' else GCSPandasIOManager(os.environ['GCS_BUCKET'])
 pyarrow_io_manager = LocalPyArrowIOManager() if ENV == 'test' else GCSPyArrowIOManager(os.environ['GCS_BUCKET'])
 
+assets = [
+    *fmp_eod_assets,
+    *fmp_unpartitioned_assets,
+    *fred_assets,
+    *regular_assets,
+    *wikipedia_assets,
+    *internal_assets,
+    *unctad_assets,
+]
+
+jobs = [
+    fmp_eod_job,
+    fmp_unpartitioned_assets_job,
+    fred_job,
+    regular_job,
+    wikipedia_job,
+    internal_job,
+    unctad_job,
+]
 defs = Definitions(
-    assets=fmp_eod_assets + fmp_unpartitioned_assets + fred_assets + regular_assets + wikipedia_assets + internal_assets,
-    jobs=[fmp_eod_job, fmp_unpartitioned_assets_job, fred_job, regular_job, wikipedia_job, internal_job],
+    assets=assets,
+    jobs=jobs,
     resources={
         "io_manager": pandas_io_manager,
         "vanilla_parquet_io_manager": pyarrow_io_manager 
