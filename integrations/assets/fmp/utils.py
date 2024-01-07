cb7f6d4612cfea5db48ecc193c1c3e158bb99d9c
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2024-01-07 15:21:56 +0100

    Set default value for fmp request util

--- integrations/assets/fmp/utils.py
+++ integrations/assets/fmp/utils.py
@@ -1,7 +1,7 @@
 import os 
 import pandas as pd
 
-def make_v4_request(route, args):
+def make_v4_request(route, args={}):
     BASE_URL = 'https://financialmodelingprep.com/api/v4/'
     default_args = {
         "apikey": os.environ['FMP_API_KEY'],
@@ -9,10 +9,13 @@ def make_v4_request(route, args):
     }
     all_args = {**default_args, **args}
     full_url = BASE_URL + route + "?" + "&".join([f"{key}={value}" for key, value in all_args.items()])
-    df = pd.read_csv(full_url)
-    return df
+    try:
+        return pd.read_csv(full_url)
+    except pd.errors.ParserError as e:
+        print('Error parsing CSV', full_url)
+        return pd.DataFrame()
 
-def make_v3_request(route, args):
+def make_v3_request(route, args={}):
     BASE_URL = 'https://financialmodelingprep.com/api/v3/'
     default_args = {
         "apikey": os.environ['FMP_API_KEY'],
@@ -20,5 +23,8 @@ def make_v3_request(route, args):
     }
     all_args = {**default_args, **args}
     full_url = BASE_URL + route + "?" + "&".join([f"{key}={value}" for key, value in all_args.items()])
-    df = pd.read_csv(full_url)
-    return df
+    try:
+        return pd.read_csv(full_url)
+    except pd.errors.ParserError as e:
+        print('Error parsing CSV', full_url)
+        return pd.DataFrame()
