ddda6741dbfd47ea73a5fc11462537061d9aa97a
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2024-01-09 12:35:52 +0100

    Add subsets_assets_path meta asset

--- integrations/assets/internal/assets.py
+++ integrations/assets/internal/assets.py
@@ -1,5 +1,7 @@
 from dagster import asset
 import pandas as pd
+import ast
+import os
 
 @asset
 def subsets_datasets() -> pd.DataFrame:
@@ -37,3 +39,34 @@ def subsets_dataset_columns() -> pd.DataFrame:
                 })
          
     return pd.DataFrame(columns)
+
+
+def find_asset_functions_in_file(file_path):
+    with open(file_path, 'r') as file:
+        node = ast.parse(file.read())
+        for function in node.body:
+            if isinstance(function, ast.FunctionDef):
+                for decorator in function.decorator_list:
+                    if (isinstance(decorator, ast.Name) and decorator.id == 'asset') or \
+                       (isinstance(decorator, ast.Call) and 
+                        isinstance(decorator.func, ast.Name) and 
+                        decorator.func.id == 'asset'):
+                        yield function.name, function.lineno
+
+
+def scan_assets_directory(relative_path='./integrations/assets'):
+    asset_functions = []
+    assets_directory = os.path.abspath(relative_path)
+    for root, dirs, files in os.walk(assets_directory):
+        for file in files:
+            if file.endswith('.py'):
+                full_path = os.path.join(root, file)
+                relative_file_path = os.path.relpath(full_path, assets_directory)
+                for function_name, line_no in find_asset_functions_in_file(full_path):
+                    asset_functions.append((relative_file_path, function_name, line_no))
+    return asset_functions
+
+@asset
+def subsets_asset_paths() -> pd.DataFrame:
+    asset_paths = scan_assets_directory()
+    return pd.DataFrame(asset_paths, columns=['path', 'function', 'line_no'])
