e1c4be64d64cee0f9d4687c4585d89086dd1e97c
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2024-01-11 14:57:08 +0100

    Return full big mac dataset (#50)

--- integrations/assets/economist/assets.py
+++ integrations/assets/economist/assets.py
@@ -30,14 +30,8 @@ source = {
         "description": "Price of a Big Mac in USD adjusted for GDP"
     }]
 }, freshness_policy=FreshnessPolicy(maximum_lag_minutes=60 * 24 * 7, cron_schedule="0 0 1 * *"))
-def economist_big_mac_index(countries):
+def economist_big_mac_index():
     url = "https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-full-index.csv"
     df = pd.read_csv(url)
-
-    # join country_code3 on iso_a3
-    df = df.merge(countries[['country_code2', 'country_code3', 'country_name']], left_on="iso_a3", right_on="country_code3", how="left")
-    df = df.rename(columns={'adj_price': 'adjusted_dollar_price'})
-    # only keep relevant columns
-    df = df[['country_code2', 'country_name', 'date', 'dollar_price', 'adjusted_dollar_price']]
     return df
     
